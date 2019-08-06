9f795ef58a943b5f4c81f7d45f18a9d0dcf76e13
nik frank <nikbfrank@gmail.com>
2019-08-06 05:20:26 -0700

    installed routing, listing detail page started, css fixes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express'); // import express
-const app = express(); // express express apllication 
+const app = express(); // express express apllication
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 4000; // port of the server. when we push on heroku he knows where to run it
 
@@ -76,13 +76,13 @@ app.use(express.json()); // function that parse the request into json (is the co
 
 const auth = (req, res, next)=>{  // next is what get u to req res
     const authHeader = req.get('Authorization') || '';  // reading the  value out of the header
-  
+
     const token = authHeader.split(' ')[1]; //string after the space (ex: Bearer RGDTHDTHDHCCG)
-  
+
     jwt.verify(token, 'jwt secret code', (err, decoded)=>{  // verify if the token is correct by decoding it with the secret code
       if( err ) return res.status(401).json({ message: 'auth failed' }); // if there is an error it means the token is unvalid we send back a 401
       else {
-        req.session = decoded; // save the decoded json with the id 
+        req.session = decoded; // save the decoded json with the id
         next(); //next will go to req res
       }
     });
@@ -93,7 +93,7 @@ connection.authenticate()             // test the database connection
     .catch((err)=> console.error(err));
 
 
-app.get('/hydrate',(req,res)=>{ 
+app.get('/hydrate',(req,res)=>{
  User.sync({force:true})        // sync = efface et creee table  // if this fail it goes to the same catch
     .then(()=>User.bulkCreate(fakeData.users)) //send fake data to the user table
     .then(()=>Listing.sync({force : true }))   // .then for calling the next lines  instead of re writing it  //if this fail it goes to the catch
@@ -104,7 +104,7 @@ app.get('/hydrate',(req,res)=>{
 });
 
 app.post('/listing',auth,(req,res)=>{ // auth is checking if we are connected (auth is a fction)
-    Listing.create({...req.body,author : req.session.id}) // post a js object 
+    Listing.create({...req.body,author : req.session.id}) // post a js object
         .then((response)=>res.status(201).json({created: response.dataValues , message:'created'})) // if the object is correct the obj is create into the database
         .catch(err => console.error(err)||res.status(500).json({message:'create listing failed'}))// else we have a 500 error that say we fucked somewhere
 
@@ -114,36 +114,43 @@ app.post('/listing',auth,(req,res)=>{ // auth is checking if we are connected (a
 app.get('/listing',(req,res)=>{
     Listing.findAll() // select every obj in the listing table
         .then(listings => res.json(listings)) //array with the dataValues
-        .catch(err => console.error(err)||res.status(500).json({message:'read listings failed'})) //error 
-        
-})
+        .catch(err => console.error(err)||res.status(500).json({message:'read listings failed'})) //error
+
+});
+
+app.get('/listing/:id',(req,res)=>{
+    Listing.findByPk(1*req.params.id) // select one listing in the listing table by id
+        .then(listing => res.json(listing)) //object with the dataValue
+        .catch(err => console.error(err)||res.status(500).json({message:'read listing failed'})) //error
+
+});
 
 app.post('/user',(req,res)=>{
     //sign up
-    const passwordHash = calculateHash(req.body.password) // send the password and call the fuction and callculate the password 
+    const passwordHash = calculateHash(req.body.password) // send the password and call the fuction and callculate the password
 
     User.create({ // take the table and create user with email and password
-        email:req.body.email, 
+        email:req.body.email,
         passwordHash,
     }).then(()=>res.status(201).json({message: 'sign up succefully'}))
-      .catch(()=>res.status(500).json({message:'putain'}))  
+      .catch(()=>res.status(500).json({message:'putain'}))
 });
 
 app.post('/login',(req,res)=>{
-    const passwordHash = calculateHash(req.body.password) // call the fuction and callculate the password 
+    const passwordHash = calculateHash(req.body.password) // call the fuction and callculate the password
     User.findOne({where:{email:req.body.email,passwordHash}}) //look for  the user with an email and password
         .then(userResponse=>{
-            if (userResponse){ // if we found the user with the mail and password 
+            if (userResponse){ // if we found the user with the mail and password
                 jwt.sign( // turn json into an encoded shit no one can fucked
                     { id: userResponse.dataValues.id}, // id of the user we found
-                    'jwt secret code', // need a secret code to encode 
+                    'jwt secret code', // need a secret code to encode
                     (err,token)=>res.json({token}) // send back  some json with the taken
                 );
 
             }else{
                 res.status(401).json({message : 'login failed '}); // 401 with no token if we did not find a user
 
-            }   
+            }
         });
 
 })
--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import './App.css';
 import MakeListing from './MakeListing.js';
 import Listings from './Listing.js'
 import Login from './Login.js';
+import ListingDetail from './ListingDetail';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom';
+
+
+class Navbar extends React.Component {
+  render(){
+    return (
+      <div className='Navbar'>
+        <Link to='/'>
+          <h1>YAD3</h1>
+        </Link>
+      </div>
+    );
+  }
+};
 
 
 class App extends React.Component{
 
   state={
     listings:[], // will contain all our info from the database (needs the same name as the model)
-    token : null, // not logged in so the token is null 
+    token : null, // not logged in so the token is null
   }
 
-  componentDidMount(){ // we cannot call it multiple time 
+  componentDidMount(){ // we cannot call it multiple time
     this.reload()
   }
-  
+
   reload = ()=>fetch('/listing') // fetch request from listing
     .then(response=>response.json()) // response in a json format
     .then(listings => this.setState({listings})) // put the response in the state
@@ -27,26 +48,42 @@ class App extends React.Component{
 
     togglePanel = () =>this.setState({panelOpen: !this.state.panelOpen})
 
-  
+
   render() {
 
     return (
-    <div className="App">
-      <div className = {'toggle-panel '+(this.state.panelOpen? 'reversed' : '')} onClick={this.togglePanel}>
-       ⌃
-      </div>
+      <Router>
+        <div className="App">
+          {this.state.token ? (
+            null
+          ) : (
+            <Navbar />
+          )}
+          <div className = {'toggle-panel '+(this.state.panelOpen? 'reversed' : '')} onClick={this.togglePanel}>
+           ⌃
+          </div>
 
-      <div style={{height:(this.state.panelOpen ? 68 : 88)+'vh'}}>
-      <Listings listings={this.state.listings}/>
-      </div>
+          <div className='content'>
+            <div style={{height:(this.state.panelOpen ? 70 : 90)+'%'}}>
+              <Switch>
+                <Route exact path='/listings' render={()=> (
+                    <Listings listings={this.state.listings}/>
+                  )}/>
+                <Route exact path='/listing/:id' component={ListingDetail} />
+                <Redirect from='/' to='/listings'/>
+              </Switch>
+            </div>
 
-        <div style={{height:(this.state.panelOpen ? 30 : 10)+'vh'}}>
-      { this.state.token?(                          // if true show the the list if not still show login 
-      <MakeListing triggerReload={this.reload} token={this.state.token}/>
-      ):( <Login onLogin={this.setToken}/>
-      )}
-      </div>
-    </div>
+            <div style={{height:(this.state.panelOpen ? 30 : 10)+'%'}}>
+              { this.state.token?(                          // if true show the the list if not still show login
+                <MakeListing triggerReload={this.reload} token={this.state.token}/>
+              ):(
+                <Login onLogin={this.setToken}/>
+              )}
+            </div>
+          </div>
+        </div>
+      </Router>
   );
 }}
 
